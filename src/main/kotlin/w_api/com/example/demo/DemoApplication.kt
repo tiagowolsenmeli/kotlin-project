f282f389f7378e2d29142dfb45a1ed85001db841
Tiago Wolsen <tiagowolsen@gmail.com>
2025-02-14 19:49:27 -0300

    insert scafolding

---
+++ src/main/kotlin/w_api/com/example/demo/DemoApplication.kt
@@ -0,0 +1,11 @@
+package w_api.com.example.demo
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class DemoApplication
+
+fun main(args: Array<String>) {
+	runApplication<DemoApplication>(*args)
+}
---
+++ src/test/kotlin/w_api/com/example/demo/DemoApplicationTests.kt
@@ -0,0 +1,13 @@
+package w_api.com.example.demo
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class DemoApplicationTests {
+
+	@Test
+	fun contextLoads() {
+	}
+
+}
